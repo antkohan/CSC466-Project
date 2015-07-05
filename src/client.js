184e08ab5470bdbb000b5ea8d8e970020a8b87b9
Anthony Kohan <antkohan@gmail.com>
2015-07-04 21:51:27 -0700

    Started adding framework for movement and physics

--- src/client.js
+++ src/client.js
@@ -3,6 +3,9 @@ window.onload = function()
 	var client = new Client();	
 
 	client.update(new Date().getTime());
+	
+	document.addEventListener("keyup", function(event) { client.onKeyUp(event); }, false);	
+	document.addEventListener("keydown", function(event) { client.onKeyDown(event); }, false);
 }
 
 var Client = function()
@@ -13,17 +16,65 @@ var Client = function()
 	this.ctx = this.canvas.getContext("2d");
 
 	this.canvas.width = this.game.world.width;
-	this.canvas.height = this.game.world.height;	
+	this.canvas.height = this.game.world.height;
+
+	this.inputSeq = 0;
+
+	this.keys = {};
+}
+
+Client.prototype.pressed = function(code)
+{
+ 	return this.keys[code];
+}
+
+Client.prototype.onKeyDown = function(event)
+{
+	var code = event.keyCode;
+	this.keys[code] = true;	
+}
+
+Client.prototype.onKeyUp = function(event)
+{
+	var code = event.keyCode;
+	this.keys[code] = false;
 }
 
 Client.prototype.update = function(time)
 {
-	this.deltaTime = this.lastFrameTime ? ( (time - this.lastFrametime)/1000.0) : 0.016;
+	this.deltaTime = this.lastFrameTime ? ( (time - this.lastFrameTime)/1000.0) : 0.016;
 	this.lastFrameTime = time;
+	
+	this.handleInput();
 
-	this.draw();
+	this.draw();	
 
         this.updateid = window.requestAnimationFrame(this.update.bind(this));
+}
+
+Client.prototype.handleInput = function()
+{
+	var input = [];
+
+	if(this.pressed(87) == true)
+	{
+		input.push('u');
+	}		
+	if(this.pressed(83) == true)
+	{
+		input.push('d');
+	}
+
+	if(input.length > 0)
+	{
+		this.inputSeq++;
+		this.game.players.p1.inputs.push({
+			inputs: input,
+			time: this.game.localTime.toFixed(3),
+			seq: this.inputSeq
+		});	
+		
+	}
 }
 
 Client.prototype.draw = function()
@@ -33,9 +84,9 @@ Client.prototype.draw = function()
 	this.ctx.fillStyle = "black";		
         this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
 
-	/* Draw Paddles  */
-	var pl = this.game.leftPaddle;
-	var pr = this.game.rightPaddle;
+	/* Draw Players  */
+	var pl = this.game.players.p1.paddle;
+	var pr = this.game.players.p2.paddle;
 	
 	this.ctx.fillStyle = "white"
 	this.ctx.fillRect(pl.x, pl.y, pl.width, pl.height);
--- src/game.js
+++ src/game.js
@@ -1,18 +1,114 @@
-var Game = function(type, options)
+var Game = function(type, parameters)
 {
 	this.type = type || "client";
-	this.options = options || {};
+	this.parameters = parameters || {};
 
+	this.player = null;
+	if(type == 'client')
+	{
+		 this.player = 1; // This should be set by the client creating the game.
+	}		
+
+	this.localTime = 0.016;
+	this.dt = new Date().getTime();
+	this.dte = new Date().getTime();
+
+	this.pdt = 0.0001;
+	this.pdte = new Date().getTime();
+	
 	this.world = 
 	{
 		width: 720,
 		height: 480
 	};
 
+	this.players =
+	{
+		p1 : new Player(this, "player 1"),
+		p2 : new Player(this, "player 2")
+	};
+
 	this.ball = new Ball();
 	this.startButton = new StartButton(this.world.width, this.world.height);
-	this.leftPaddle = new Paddle(this.world.width, this.world.height, "left");
-	this.rightPaddle = new Paddle(this.world.width, this.world.height, "right");
+
+	this.createTimer();
+	this.createPhysicsTimer();
+}
+
+Game.prototype.updatePhysics = function()
+{
+	if(this.type == 'client')
+	{
+		var y = this.processInput(this.players.p1)
+		this.players.p1.paddle.y += y;
+	}	
+}
+
+Game.prototype.processInput = function(player)
+{
+	var y = 0;
+
+	var numSeq = player.inputs.length;
+
+	for(var i = 0; i < numSeq; i++)
+	{
+		if(player.inputs[i].seq <= player.lastInputSeq) 
+			continue;
+
+		var input = player.inputs[i].inputs;
+		var numInputs = input.length;
+		
+		for(var j = 0; j < numInputs; j++)
+		{	
+			var key = input[j];
+			if(key == 'u'){
+				y--;	
+			}
+			if(key == 'd'){
+				y++;
+			}
+		}
+	}	
+		
+	if(numSeq > 0)
+	{
+		player.lastInputTime = player.inputs[numSeq-1].time;
+		player.lastInputSeq = player.inputs[numSeq-1].seq;
+	}
+		
+	return y;
+}
+
+Game.prototype.createPhysicsTimer = function()
+{
+	setInterval(function(){
+		this.pdt = (new Date().getTime() - this.pdte)/1000.0;
+		this.pdte = new Date().getTime();
+		this.updatePhysics();
+	}.bind(this), 15); 
+}
+
+Game.prototype.createTimer = function()
+{
+	setInterval(function(){
+		this.dt = new Date().getTime() - this.dte;
+		this.dte = new Date().getTime();
+		this.localTime += this.dt / 1000.0;	
+	}.bind(this), 4);
+}
+
+var Player = function(gameInstance, port)
+{
+	this.game = gameInstance;
+
+	this.lastInputSeq = -1;
+	this.lastInputTime = -1;
+	this.inputs = [];
+	
+	if(port == "player 1")
+		this.paddle = new Paddle(this.game.world.width, this.game.world.height, "left");
+	else 
+		this.paddle = new Paddle(this.game.world.width, this.game.world.height, "right");
 }
 
 var Paddle = function(worldWidth, worldHeight, side)
